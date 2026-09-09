7f84a9c319b4f5f1c6fb5d1d91d5086a48fc84a4
Dante Zulli <dantezulli2004@gmail.com>
2026-09-09 19:43:35 -0300

    update 2026-09-09 19:43:35

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -35,6 +35,11 @@ end)
 
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
+hl.env("GTK_THEME", "Adwaita:dark")
+hl.env("QT_STYLE_OVERRIDE", "kvantum")
+hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
+hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
+hl.env("ELECTRON_OZONE_PLATFORM_HINT", "auto")
 
 -------------------
 ---- LOOK AND FEEL ----
